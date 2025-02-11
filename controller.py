94491f079180b3a179506d7261a974513175a460
Bill Le <choangbill.le@gmail.com>
2025-02-11 17:06:33 -0500

    controller class outline

--- controller.py
+++ controller.py
@@ -7,3 +7,9 @@ class Controller:
     def print_hello_world(self):
         self.person.change_name()
         print(self.person.get_name())
+
+    def pdf_to_text(self, path_to_pdf):
+        pass
+
+    def text_to_braille(self, text):
+        pass
