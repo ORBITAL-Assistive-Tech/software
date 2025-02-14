68ca46c79bb70de7f55a487a61cc75cc2e5f493b
metalcupcake5 <imcoolcom5@gmail.com>
2025-02-13 20:08:23 -0500

    remove unnecessary newline addition

--- controller.py
+++ controller.py
@@ -19,7 +19,7 @@ class Controller:
 
     def upload_text_file(self, path):
         with open(path, "r") as f:
-            text = "\n".join(f.readlines())
+            text = "".join(f.readlines())
             braille = brl.translate(text)
             braille_file = BrailleFile(text, braille, path)
             self.reader.add_document(braille_file)
