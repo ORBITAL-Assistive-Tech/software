255b2c20eebb9a28e1816164ca85a289d8090f6b
metalcupcake5 <imcoolcom5@gmail.com>
2025-02-13 20:26:50 -0500

    automatically read documents on run

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from view import View
 from controller import Controller
 from model import Reader
+import os
+import warnings
 
 
 def main():
     reader = Reader()
     control = Controller(reader)
-    control.load_braille_file("documents/book1.txt")
-    control.upload_text_file("documents/test.txt")
+    for f in os.listdir("documents"):
+        if not os.path.isfile(f"documents/{f}"):
+            continue
+        _, extension = os.path.splitext(f)
+        if extension == ".pdf":
+            pass
+        if extension == ".txt":
+            control.upload_text_file(f"documents/{f}")
+            continue
+        if extension == ".json":
+            control.load_braille_file(f"documents/{f}")
+            continue
+        warnings.warn(f"{f} did not have a recognized file extension")
+    for d in control.reader.documents:
+        print(d.id)
 
 
 if __name__ == "__main__":
