f727f625aad1da788676c3280f555c5a4b726cfe
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2025-04-24 20:09:28 -0400

    implement text to braille/brf methods

--- controller.py
+++ controller.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append("/home/koroko/Workspace/pybrl")
 
 import pybrl as brl
+import louis
 
 # /home/koroko/Workspace/pybrl
 
@@ -21,7 +22,58 @@ class Controller:
         self.reader = reader
 
     def text_to_braille(self, text):
-        return brl.translate(text)
+        """
+        Convert text into 40 character lines of braille
+
+        Args:
+            text (str): text to convert into braille
+                assumed to have only line feeds (\n) for new lines
+
+        Returns:
+            A list of lines, which contain up to 40 strings, each representing a character
+        """
+        paragraphs = text.split("\n\n")
+        braille_list = []
+        for l in paragraphs:
+            braille = brl.translate(l)
+            paragraph = []
+            for word in braille:
+                for letter in word:
+                    paragraph.append(letter)
+                paragraph.append("")
+            paragraph = paragraph[:-1]
+            while len(paragraph) > 0:
+                # removes leading spaces from line
+                # not sure if this is the right move so commented out
+                # while len(paragraph[0]) < 1:
+                #    paragraph.pop(0)
+                braille_list.append(paragraph[0:40])
+                paragraph = paragraph[40:]
+            braille_list.append([])
+
+        braille_list = braille_list[:-1]  # remove trailing empty line
+
+        return braille_list
+
+    def text_to_brf(self, text):
+        """
+        Convert text into braille ready format (BRF)
+
+        Args:
+            text (str): text to convert into BRF
+
+        Returns:
+            A string with the BRF translation
+        """
+        string = louis.translateString(
+            ["en-us-brf.dis", "en-us-g2.ctb"],
+            text,
+        )
+        brf_lines = []
+        while len(string) > 0:
+            brf_lines.append(string[0:40])
+            string = string[40:]
+        return "\n".join(brf_lines)
 
     def upload_text_file(self, path):
         with open(path, "r") as f:
